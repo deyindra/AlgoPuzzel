ff5121b6a693cd2466b598eeda3fe36f9c037370
deyindra <Cpwr@1234>
2017-06-05 11:29:23 -0700

    draft

--- src/main/java/org/idey/algo/datastructure/list/undoredo/UndoRedoList.java
+++ src/main/java/org/idey/algo/datastructure/list/undoredo/UndoRedoList.java
@@ -1,10 +1,16 @@
 package org.idey.algo.datastructure.list.undoredo;
 
+import org.idey.algo.oops.UndoRedo;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UndoRedoList<T> {
+    private final Logger logger = Logger.getLogger(UndoRedo.class.getName());
     private List<T> list;
     private Stack<AbstractListCommand<T>> undoStack;
     private Stack<AbstractListCommand<T>> redoStack;
@@ -15,6 +21,8 @@ public class UndoRedoList<T> {
         undoStack = new Stack<>();
         redoStack = new Stack<>();
         currentCommand = null;
+        logger.addHandler(new ConsoleHandler());
+        logger.setLevel(Level.ALL);
     }
 
     private void execute(int index, T object, Operation op){
@@ -28,6 +36,9 @@ public class UndoRedoList<T> {
         }
         undoStack.push(prevCommand);
         currentCommand.redo(list);
+        logger.info(String.format("Undo Stack %s", undoStack.toString()));
+        logger.info(String.format("Redo Stack %s", redoStack.toString()));
+
     }
 
     public void add(T object){
@@ -59,6 +70,8 @@ public class UndoRedoList<T> {
         currentCommand.undo(list);
         redoStack.push(currentCommand);
         currentCommand = undoStack.pop();
+        logger.info(String.format("Undo Stack %s", undoStack.toString()));
+        logger.info(String.format("Redo Stack %s", redoStack.toString()));
     }
 
     public void redo(){
@@ -69,6 +82,8 @@ public class UndoRedoList<T> {
         undoStack.push(prevCommand);
         currentCommand = redoStack.pop();
         currentCommand.redo(list);
+        logger.info(String.format("Undo Stack %s", undoStack.toString()));
+        logger.info(String.format("Redo Stack %s", redoStack.toString()));
     }
 
     public boolean canUndo(){
@@ -88,6 +103,23 @@ public class UndoRedoList<T> {
 
     private enum Operation{
         ADD, UPDATE, DELETE
+    }
+
+    public static void main(String[] args) {
+        UndoRedoList<Integer> list = new UndoRedoList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.remove((Integer)2);
+        while (list.canUndo()){
+            list.undo();
+        }
+        System.out.println(list);
+        while (list.canRedo()){
+            list.redo();
+        }
+        System.out.println(list);
+
     }
 
 }
